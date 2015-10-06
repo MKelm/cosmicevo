ce972606085b02ed06085d0a141f91ea054c7b07
Martin Kelm <martinkelm@shrt.ws>
2015-10-06 11:54:07 +0000

    more changes to get correct scroll animation test

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-// init controller
-var controller = new ScrollMagic.Controller();
-
-// create a scene
-new ScrollMagic.Scene({
-        trigger: "#trigger1",
-        duration: 100,  // the scene should last for a scroll distance of 100px
-        offset: 50      // start this scene after scrolling for 50px
-    })
-    .setTween("#animate1", 0.5, { backgroundColor: "green", scale: 2.5 })
-    .addIndicators({name: "1 (duration: 0)"})
-    .addTo(controller);
+$(document).ready(function() {
+    
+    // init controller
+    var controller = new ScrollMagic.Controller();
+    
+    // create a scene
+    new ScrollMagic.Scene({
+            trigger: "#trigger1",
+            duration: 100,  // the scene should last for a scroll distance of 100px
+            offset: 50      // start this scene after scrolling for 50px
+        })
+        .setTween("#animate1", 0.5, { backgroundColor: "#ccc", scale: 2.5 })
+        .addIndicators({name: "1 (duration: 100)"})
+        .addTo(controller);
+        
+    new ScrollMagic.Scene({
+            trigger: "#trigger2",
+            duration: 100,  // the scene should last for a scroll distance of 100px
+            offset: 200      // start this scene after scrolling for 50px
+        })
+        .setTween("#animate1", 0.5, { backgroundColor: "#222", scale: 1 })
+        .addIndicators({name: "2 (duration: 100)"})
+        .addTo(controller);
+    
+});
